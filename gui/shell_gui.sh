b77dc3247c50aca1a1b090f1119e5852d2fb2c6e
k8si <kate@k8si.com>
2013-08-12 19:35:39 -0400

    updated shell_gui.sh with the rest of the parameters

--- gui/shell_gui.sh
+++ gui/shell_gui.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
 sel=$(
-dialog --stdout --title "Parameter Selection" \
-    --checklist "Choose F0 alg:" 15 40 5 \
-    1 Snack off \
-    2 Praat off \
-    3 SHR off )
+dialog \
+    --stdout --keep-window --title "Parameter Selection" --begin 2 2 \
+    --checklist "F0:" 10 40 20 \
+    1 "F0 (Snack)" off \
+    2 "F0 (Praat)" off \
+    3 "F0 (SHR)" off \
+    --and-widget --clear --begin 2 2 \
+    --checklist "Formants:" 10 40 20 \
+    1 "F1, F2, F3, F4 (Snack)" off \
+    2 "F1, F2, F3, F4 (Praat)" off \
+    --and-widget --clear --begin 4 4 \
+    --checklist "Other Measurements:" 20 40 20 \
+    1 "H1, H2, H4" off \
+    2 "A1, A2, A3" off \
+    3 "H1*-H2*, H2*-H4*" off \
+    4 "H1*-A1*, H1*-A2*, H1*-A3*" off \
+    5 Energy off \
+    6 CPP off \
+    7 "Harmonic to Noise Ratios - HNR" off \
+    8 "Subharmonic to Harmonice Ratio - SHR" off
+    )
 
 case $? in
 0)
 echo "$sel" > selection.txt ;;
 1)
-echo "You have pressed Cancel" ;;
+echo "Cancelled." ;;
 255)
-echo "Box closed" ;;
+echo "Box closed." ;;
 esac
