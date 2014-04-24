f1b56a5197ef41f858fb00927b8352fdd890b64b
k8si <ksilvers@umass.edu>
2014-04-24 09:44:27 -0400

    fixed a bug with settings

--- bin/VoiceSauce.m
+++ bin/VoiceSauce.m
@@ -1,10 +1,12 @@
-function VoiceSauce(indir, outdir)
+function VoiceSauce(indir, outdir, settingsfile)
 % simply runs batch_process and then OutputToText
 % indir - the directory where wave files are stored
 % outdir - the directory where *.mat files should be stored
 % OutputToText parameters are specified in "settings/getOutputSettings.m"
 
-[instance_data, err] = batch_process(indir, outdir);
+settings = getSettings(settingsfile);
+
+[instance_data, err] = batch_process(indir, outdir, settings);
 assert (err == 0, 'Error: Something went wrong in batch_process');
 
 err = OutputToText(instance_data);
--- sauce.m
+++ sauce.m
@@ -4,19 +4,19 @@ vs_path = genpath('.');
 addpath(vs_path);
 
 arg_list = argv();
-usage = 'octave -qf sauce.m [full-path-to-wavdir] [full-path-to-matdir] [path-to-settings]';
+usage = 'octave -qf sauce.m [full-path-to-wavdir] [full-path-to-matdir] [path-to-settings-file]';
 
 if (nargin == 3)
 	printf('\n ~~~ OpenSauce ~~~ \n\n');
 	indir = arg_list{1};
 	outdir = arg_list{2};
-	settingsdir = arg_list{3};
+	settingsfile = arg_list{3};
 	%-- GUI stuff --%
 	% unix('gui/shell_gui.sh');
 	% unix('python gui/get_params.py');
 	% -- -- -- %
-	printf('wav dir: [%s]; mat dir: [%s]; settings dir: [%s]\n\n', indir, outdir, settingsdir);
-	VoiceSauce(indir, outdir, settingsdir);
+	printf('wav dir: [%s]; mat dir: [%s]; settings file: [%s]\n\n', indir, outdir, settingsfile);
+	VoiceSauce(indir, outdir, settingsfile);
 else
 	printf('error: usage: %s', usage);
 	exit(-1);
